0bde26417d222f17f97de8e23a3bedda6c710edd
karthik g k <kar221694@gmail.com>
2023-04-13 19:24:00 +0530

    added socket.io change

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ let path = location.pathname.split('/');
 let username = path[1].split("%20").join(" ");
 let room = path[2].split("%20").join(" ");
 
-var socket = io.connect("http://localhost:3000",{
+var socket = io.connect("https://chat-app-10h9.onrender.com/",{
   query:{
     username: username,
     room : room
